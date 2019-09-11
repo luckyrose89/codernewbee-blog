edd8c19a730572166f5bef8c4a170e8dc4689710
avidrutham <divyamathur89@gmail.com>
2019-09-11 21:50:53 +0530

    add blog listing query to blog component

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,15 +19,6 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        commonmark: true,
-        footnotes: true,
-        pedantic: true,
-        gfm: true,
-        plugins: [],
-      },
-    },
+    "gatsby-transformer-remark",
   ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,36 @@
 import React from "react"
 import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>Blog</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge, index) => {
+          return (
+            <li key={index}>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
