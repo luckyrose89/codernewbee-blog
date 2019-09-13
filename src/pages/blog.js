0a705e35fa4a9adf1acc67f19def64953d66c97c
avidrutham <divyamathur89@gmail.com>
2019-09-13 23:52:37 +0530

    add and complete blog template

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -13,6 +13,9 @@ const BlogPage = () => {
               date
             }
             excerpt
+            fields {
+              slug
+            }
           }
         }
       }
@@ -26,9 +29,11 @@ const BlogPage = () => {
         {data.allMarkdownRemark.edges.map((edge, index) => {
           return (
             <li key={index}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
-              <p>{edge.node.excerpt}</p>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
+                <p>{edge.node.excerpt}</p>
+              </Link>
             </li>
           )
         })}
